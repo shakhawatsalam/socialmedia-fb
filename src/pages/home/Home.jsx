ffc0998d66defca4b5f52bf58c3aa8708fd0cd75
shakhawatsalam <shakhawatsalam726@gmail.com>
2022-07-11 12:52:06 +0600

    working no dividing side

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Home.css'
+
+const Home = () => {
+    return (
+        <div className='Home'>
+            <div className='profileSide'>profile</div>
+            <div className='postSide'>Posts</div>
+            <div className='RightSide'>RightSide</div>
+        </div>
+    );
+};
+
+export default Home;
