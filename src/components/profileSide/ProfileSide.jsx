b7682b74129f10e55a00d019479132174367b6ad
shakhawatsalam <shakhawatsalam726@gmail.com>
2022-07-11 13:01:16 +0600

    working on profile side

---
+++ src/components/profileSide/ProfileSide.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ProfileSide = () => {
+    return (
+        <div className='ProfileSide'>
+            ProfileSideHello world
+        </div>
+    );
+};
+
+export default ProfileSide;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import ProfileSide from '../../components/profileSide/ProfileSide';
 import './Home.css'
 
 const Home = () => {
     return (
         <div className='Home'>
-            <div className='profileSide'>profile</div>
+            <ProfileSide/>
             <div className='postSide'>Posts</div>
             <div className='RightSide'>RightSide</div>
         </div>
