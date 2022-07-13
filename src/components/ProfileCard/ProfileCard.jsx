b9bafdf2ff404a5596442720a5cb5745a0e6a18b
shakhawatsalam <shakhawatsalam726@gmail.com>
2022-07-13 11:16:37 +0600

    working on profile card

---
+++ src/components/ProfileCard/ProfileCard.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import "./ProfileCard.css";
+import Cover from "../../img/cover.jpg";
+import Profile from "../../img/profileImg.jpg";
+
+const ProfileCard = () => {
+  return (
+    <div className='ProfileCard'>
+      <div className='ProfileImages'>
+        <img src={Cover} alt='' />
+        <img src={Profile} alt='' />
+      </div>
+      <div className='ProfileName'>
+        <span>Shawon</span>
+        <span>Wev developer</span>
+      </div>
+
+      {/* follows and following  */}
+      <div className='followStatus'>
+        <hr />
+        <div>
+          <div className='follow'>
+            <span>6000</span>
+            <span>Followings</span>
+          </div>
+          <div className='vl'></div>
+          <div className='follow'>
+            <span>1</span>
+            <span>Followers</span>
+          </div>
+        </div>
+        <hr />
+      </div>
+    </div>
+  );
+};
+
+export default ProfileCard;
