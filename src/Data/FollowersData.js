abaf9df91bb89b6e307bff08aa23f9f228dc91a2
shakhawatsalam <shakhawatsalam726@gmail.com>
2022-07-16 10:42:04 +0600

    followers side is almost done

--- src/Data/FollowersData.js
+++ src/Data/FollowersData.js
@@ -1,1 +1,11 @@
-import img1 from ""
+import img1 from "../img/img1.png";
+import img2 from "../img/img2.png";
+import img3 from "../img/img3.png";
+import img4 from "../img/img4.jpg";
+
+export const Followers = [
+    { name: "Irfan", username: "Irfan Chy", img: img1 },
+    { name: "Nishu", username: "Scarlet Nishu", img: img2 },
+    { name: "Helal", username: "AB de Helal", img: img3 },
+    { name: "Elon", username: "Elon Musk", img: img4 }
+];
