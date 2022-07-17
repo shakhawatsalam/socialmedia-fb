1755e63138c615814d6924c69f3484b47fe6e573
shakhawatsalam <shakhawatsalam726@gmail.com>
2022-07-17 17:56:19 +0600

    working on RightSide trend card section

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,16 +1,17 @@
-import React from 'react';
-import PostSide from '../../components/PostSide/PostSide';
-import ProfileSide from '../../components/profileSide/ProfileSide';
-import './Home.css'
+import React from "react";
+import PostSide from "../../components/PostSide/PostSide";
+import ProfileSide from "../../components/profileSide/ProfileSide";
+import RightSide from "../../components/RightSide/RightSide";
+import "./Home.css";
 
 const Home = () => {
-    return (
-        <div className='Home'>
-            <ProfileSide/>
-            <PostSide/>
-            <div className='RightSide'>RightSide</div>
-        </div>
-    );
+  return (
+    <div className='Home'>
+      <ProfileSide />
+      <PostSide />
+      <RightSide />
+    </div>
+  );
 };
 
 export default Home;
