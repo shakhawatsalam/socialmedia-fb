a89dc0bf387e9de4be28931a60fb542b64536212
shakhawatsalam <shakhawatsalam726@gmail.com>
2022-07-17 11:40:17 +0600

    working on post compontnt

---
+++ src/components/Posts/Posts.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Posts.css";
+import { PostData } from "../../Data/PostData";
+function Posts() {
+    return <div className='Posts'>
+      {}
+  </div>;
+}
+
+export default Posts;
