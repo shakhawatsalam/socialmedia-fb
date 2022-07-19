ff67041306f23e9e64c3d5c4079bc6c3f223df92
shakhawatsalam <shakhawatsalam726@gmail.com>
2022-07-19 18:10:36 +0600

    working on Auth page style

--- src/pages/Auth/Auth.jsx
+++ src/pages/Auth/Auth.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 import './Auth.css'
+import Logo from '../../img/logo.png'
 
 const Auth = () => {
   return (
-    <div>Auth</div>
+    <div className="Auth">
+      <div className="a_left">
+        <img src={Logo} alt="" />
+        <div className="Webname">
+          <h1>Sk Media</h1>
+          <h6>Explore The ideas throughout the world</h6>
+        </div>
+      </div>
+      <h1>Form</h1>
+    </div>
   )
 }
 
