62c41038c26d1d2ac0f78983a09881115911f500
shakhawatsalam <shakhawatsalam726@gmail.com>
2022-07-26 01:14:03 +0600

    like and unlike functionality is alomst done

--- src/api/PostRequest.js
+++ src/api/PostRequest.js
@@ -3,5 +3,5 @@ import axios from 'axios'
 
 const API = axios.create({ baseURL: 'http://localhost:5000' });
 
-// export const getTimeLinePosts = (id) => API.get(`/posts/${id}/timeline`)
 export const getTimelinePosts = (id) => API.get(`/posts/${id}/timeline`);
+export const likePost = (id, userId) => API.put(`post/${id}/like`, { userId: userId })
--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -6,13 +6,19 @@ import Heart from "../../img/like.png";
 import NotLike from "../../img/notlike.png";
 import { useSelector } from "react-redux";
 import { useState } from "react";
+import { likePost } from "../../api/PostRequest";
 
 const Post = ({ data }) => {
   const { user } = useSelector((state) => state.authReducer.authData);
   const [liked, setLiked] = useState(data.likes.includes(user._id));
   const [likes, setLikes] = useState(data.likes.length);
 
-  //
+  //handle Like
+  const handleLike = () => {
+    setLiked((prev) => !prev);
+    likePost(data._id, user._id)
+    liked ? setLikes((prev) => prev - 1) : setLikes((prev) => prev + 1);
+  };
   return (
     <div className='Post'>
       <img
@@ -24,7 +30,7 @@ const Post = ({ data }) => {
           src={liked ? Heart : NotLike}
           alt=''
           style={{ cursor: "pointer" }}
-          onclick={handleLike}
+          onClick={handleLike}
         />
         <img src={Comment} alt='' />
         <img src={Share} alt='' />
